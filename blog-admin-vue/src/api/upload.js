15e8fdd4d01afbc418340740dfc8c55d6a3a763d
joye <545593725@qq.com>
2020-09-24 23:02:23 +0800

    修改上传功能

--- blog-admin-vue/src/api/upload.js
+++ blog-admin-vue/src/api/upload.js
@@ -1,16 +1,22 @@
 import http from '@/util/http'
 import axios from 'axios'
+import { Message } from 'element-ui'
 
 export function getUploadToken() {
   return http.get('/qiniu/token');
 }
 
-export function upload(name, file) {
+/**
+ * 上传文件到七牛
+ * @param file 文件
+ * @param name 文件名
+ */
+export function upload(file, name) {
   return getUploadToken().then(res => {
     let param = new FormData();
     param.append("token", res.data.data);
     param.append("file", file);
-    param.append("resource_key", name);
+    param.append("key", name);
     return axios({
       url: window.QINIU.UPLOAD_URL,
       method: 'post',
@@ -22,6 +28,10 @@ export function upload(name, file) {
       // TODO 错误判断
       res.data.url = window.QINIU.CDN_PREFIX + res.data.key;
       return res;
+    }).catch(error => {
+      console.error('上传文件出错', error.response);
+      Message.error("上传错误：" + error.response.data.error);
+      return error;
     })
   })
 }
--- blog-admin-vue/src/views/Upload.vue
+++ blog-admin-vue/src/views/Upload.vue
@@ -1,17 +1,12 @@
 <template>
   <div>
-    <div
-      ref="paste"
-      type="text"
-      contenteditable="true"
-      @paste.stop.prevent="pasteImg($event)"
-    ></div>
-    <el-input
-      id="input"
-      type="textarea"
-      placeholder="请粘贴图片"
-      class="text"
-    ></el-input>
+    <div>
+      <el-input v-model="dir" slot="prepend" placeholder="请指定目录" />
+      <el-input slot="append" v-model="fileName" placeholder="请指定文件名" />
+    </div>
+    <div class="paste-div" type="text" @paste="pasteImg($event)">
+      先点击此处，再粘贴图片
+    </div>
     <span>预览</span>
     <el-image
       fit="contain"
@@ -21,13 +16,13 @@
     ></el-image>
     <el-button type="primary" @click="upload">开始上传</el-button>
     <p>{{ "七牛外链: " + url }}</p>
+    <el-button v-show="url">复制</el-button>
   </div>
 </template>
 
 <script>
 import { upload } from "@/api/upload";
 import { Message, MessageBox } from "element-ui";
-import ClipboardJS from "clipboard";
 
 export default {
   data() {
@@ -35,98 +30,28 @@ export default {
       base64: "",
       file: "",
       url: "",
+      dir: "",
+      fileName: "",
     };
   },
   methods: {
     upload() {
-      upload(this.file)
+      if (!this.dir) {
+        this.$message.error("请指定目录");
+        return;
+      }
+      if (!this.fileName) {
+        this.$message.error("请指定文件名");
+        return;
+      }
+      upload(this.file, this.dir + "/" + this.fileName)
         .then((res) => {
           // 上传完成后，显示url
           this.url = res.data.url;
-        })
-        .catch((error) => {
-          this.$message.error("上传错误：" + error.response.status);
         });
     },
-    readFile(file) {
-      var reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        if (reader.result) {
-          //预览文件
-          this.base64 = reader.result;
-        }
-      };
-    },
-    pasteImg(e) {
-      console.log("获得剪切板的内容", e);
-      const cbd = e.clipboardData;
-      const ua = window.navigator.userAgent;
-      // 如果是 Safari 直接 return
-      if (!(e.clipboardData && e.clipboardData.items)) {
-        return;
-      }
-      if (
-        cbd.items &&
-        cbd.items.length === 2 &&
-        cbd.items[0].kind === "string" &&
-        cbd.items[1].kind === "file" &&
-        cbd.types &&
-        cbd.types.length === 2 &&
-        cbd.types[0] === "text/plain" &&
-        cbd.types[1] === "Files" &&
-        ua.match(/Macintosh/i) &&
-        Number(ua.match(/Chrome\/(\d{2})/i)[1]) < 49
-      ) {
-        return;
-      }
-      for (let i = 0; i < cbd.items.length; i++) {
-        let item = cbd.items[i];
-        if (item.kind == "file") {
-          // blob 就是从剪切板获得的文件，可以进行上传或其他操作
-          const blob = item.getAsFile();
-          console.log("获得blob", blob);
-          if (blob.size === 0) {
-            return;
-          }
-          const reader = new FileReader();
-          const imgs = new Image();
-          imgs.file = blob;
-          reader.onload = (function (aImg) {
-            return function (e) {
-              console.log("获得粘贴的结果", e.target.result);
-              aImg.src = e.target.result;
-            };
-          })(imgs);
-          reader.readAsDataURL(blob);
-          this.$refs.paste.appendChild(imgs);
-        }
-      }
-    },
-  },
-  mounted() {
-    document.getElementById("input").addEventListener("paste", (event) => {
-      console.log("paste event");
-      var clipdata = event.clipboardData || window.clipboardData;
-      window.c = clipdata;
-      console.log(clipdata);
-
-      var clipboard = new ClipboardJS("#input");
-
-      clipboard.on("success", function (e) {
-        console.info("Action:", e.action);
-        console.info("Text:", e.text);
-        console.info("Trigger:", e.trigger);
-        console.log(e);
-        e.clearSelection();
-      });
-
-      clipboard.on("error", function (e) {
-        console.error("Action:", e.action);
-        console.error("Trigger:", e.trigger);
-      });
-      return;
-
+    pasteImg(event) {
+      console.debug("paste", event);
       if (!(event.clipboardData && event.clipboardData.items)) {
         return;
       }
@@ -134,14 +59,23 @@ export default {
 
       if (item.kind === "string") {
         item.getAsString((str) => {
-          alert("paste string?");
+          this.$message.error("无法粘贴文本");
+          console.log("pastee", str);
         });
       } else if (item.kind === "file") {
         this.file = item.getAsFile();
-        this.readFile(this.file);
+        var reader = new FileReader();
+        reader.readAsDataURL(this.file);
+        reader.onload = () => {
+          if (reader.result) {
+            //预览文件
+            this.base64 = reader.result;
+          }
+        };
       }
-    });
+    },
   },
+  mounted() {},
 };
 </script>
 
@@ -154,9 +88,21 @@ export default {
 
 .image {
   margin-top: 5px;
-  width: 150px;
-  height: 100px;
+  width: 200px;
+  height: 160px;
   display: block;
   margin-bottom: 20px;
+}
+
+.paste-div {
+  margin:20px 0;
+  color: #c0c4cc;
+  width: 200px;
+  height: 160px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 15px;
+  background: #f5f7fa;
 }
 </style>
